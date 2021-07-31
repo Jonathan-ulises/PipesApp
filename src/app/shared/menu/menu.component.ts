0e6a9db4592b06ec0f9b91a89500553939aaa06b
Jonathan <jonulisanpar@gmail.com>
2021-07-31 14:00:38 -0500

    Seccion 10: Pipes de Angular
    Capitulo 138: Camniar las rutas utilizando el MenuBar
    En este caoitulo se aÃ±adieron los items al menu bar, ademas de agregar
    el sistemas de rutas.

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -16,30 +16,35 @@ export class MenuComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    //Lista de items del menu bar
     this.items = [
       {
-          label: 'File',
-          items: [{
-                  label: 'New', 
-                  icon: 'pi pi-fw pi-plus',
-                  items: [
-                      {label: 'Project'},
-                      {label: 'Other'},
-                  ]
-              },
-              {label: 'Open'},
-              {label: 'Quit'}
-          ]
+        label: 'Pipes de Angular',
+        icon: 'pi pi-desktop',
+        items: [
+          {
+            label: 'Textos y Fechas',
+            icon: 'pi pi-align-left',
+            routerLink: '/'
+          },
+          {
+            label: 'Numero',
+            icon: 'pi pi-dollar',
+            routerLink: 'numeros'
+          },
+          {
+            label: 'No Comunes',
+            icon: 'pi pi-globe',
+            routerLink: 'no-comunes'
+          },
+        ]
       },
       {
-          label: 'Edit',
-          icon: 'pi pi-fw pi-pencil',
-          items: [
-              {label: 'Delete', icon: 'pi pi-fw pi-trash'},
-              {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
-          ]
+        label: 'Pipes Personalizados',
+        icon: 'pi pi-cog'
       }
-  ];
+    ];
   }
 
 }
