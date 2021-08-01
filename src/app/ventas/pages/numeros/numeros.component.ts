62af54f52e46604d15d1d7a44f468865cd1a88dd
Jonathan Sanchez <jonulisanpar@gmail.com>
2021-08-01 18:55:24 -0500

    Seccion 10: Pipes de Angular
    Capitulo 144: DecimalPipe
    En este capitulo se vio el funcionamiento del DecimalPipe (number). Este
    pipe es capas de dar formato visual a los numeros, limitar y redondear
    decimales y localizar la representacion de cifras.

--- src/app/ventas/pages/numeros/numeros.component.ts
+++ src/app/ventas/pages/numeros/numeros.component.ts
@@ -6,11 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NumerosComponent implements OnInit {
+export class NumerosComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ventasNetas : number = 2987123.6775;
+  porcentaje  : number = 0.48;
 }
