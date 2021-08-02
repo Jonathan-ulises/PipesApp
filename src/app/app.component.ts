80891b6d6667fef81b4e0e406c17f780f1c0ee35
Jonathan Sanchez <jonulisanpar@gmail.com>
2021-08-01 19:39:28 -0500

    Seccion 10: Pipes de Angular
    Capitulo 146: PrimeNG - FieldSet
    En este capitulo se vio como crear FieldSet de PrimeNG, estos contendran
    informacion nde Pipes poco comunes de ver. Estos componentes de primeNG
    tienen una animacion de compresion y descompresion. Ademas de activo la
    configuracion de rippple para que agrege la animacion de splash a las
    interacciones con los botones.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from "primeng/api";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   nombre: string = 'joNaTHan UlISEs';
   valor: number = 1000;
   obj = {
     nombre: 'Angular'
   };
+
+  constructor( private primeNGConfig: PrimeNGConfig){}
+
+  ngOnInit(): void {
+    this.primeNGConfig.ripple = true; 
+  }
 }
  
