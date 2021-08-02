fc3110d8553b8bf2693bdea7beb92123f23d7b07
Jonathan <jonulisanpar@gmail.com>
2021-08-02 14:47:31 -0500

    Seccion 11: Pipes Personalizados
    Capitulo 161: Llenar un PrimeTable con data
    En este capitulo se uso el Table de PrimeNG, ahora muestra datos de una
    lista de objetos que se creo en el componente.ts

---
+++ src/app/ventas/interfaces/ventas.interfaces.ts
@@ -0,0 +1,12 @@
+export enum Color {
+    ROJO,
+    NEGRO,
+    AZUL,
+    VERDE
+}
+
+export interface Heroe {
+    nombre: string;
+    vuela: boolean;
+    color: Color;
+}
--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -9,9 +10,37 @@ import { Component } from '@angular/core';
 export class OrdenarComponent {
 
   upperLower: boolean = false;
+  heroes: Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.AZUL
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.NEGRO
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.VERDE
+    },
+    {
+      nombre: 'Daredevil',
+      vuela: false,
+      color: Color.ROJO
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.VERDE
+    }
+  ]
 
   changeUpperLower(): void {
     this.upperLower = !this.upperLower;
   }
+
 
 }
